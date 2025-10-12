f6793d27609b5f966c304cf3b8f710c522a1035f
Keanu Heim <Keanu.Heim@gmx.net>
2025-10-12 14:48:58 +0200

    Entity class

---
+++ src/main/java/PlaceEntry.java
@@ -0,0 +1,22 @@
+public class PlaceEntry {
+
+    String name;
+    String description;
+    int rating;
+    int like;
+    int dislike;
+    String imageLink;
+
+    PlaceEntry(){
+    }
+
+    public PlaceEntry(String name, String description, int rating, int like, int dislike, String imageLink) {
+        this.name = name;
+        this.description = description;
+        this.rating = rating;
+        this.like = like;
+        this.dislike = dislike;
+        this.imageLink = imageLink;
+    }
+
+}
