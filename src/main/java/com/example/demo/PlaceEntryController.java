1193c2840e85725353c36daf3ea874d151f48e21
Jonathan Hanebutt <s0598131@htw-berlin.de>
2025-10-13 11:09:03 +0200

    Route für das Mapping in dem PlaceEntryController hinzugefügt

--- src/main/java/com/example/demo/PlaceEntryController.java
+++ src/main/java/com/example/demo/PlaceEntryController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class PlaceEntryController {
 
-    @GetMapping
+    @GetMapping("/places")
     public String allPlaces() {
         return "All places";
     }
