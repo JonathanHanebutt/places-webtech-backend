fb1589cc1cd33820c9542213cb81e8c9327da72e
Jonathan Hanebutt <s0598131@htw-berlin.de>
2025-11-21 16:38:02 +0100

    Cross origin hinzufügen

--- src/main/java/com/example/demo/PlaceEntryController.java
+++ src/main/java/com/example/demo/PlaceEntryController.java
@@ -1,10 +1,12 @@
 package com.example.demo;
 
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins = "*")
 public class PlaceEntryController {
 
     @GetMapping("/places")
