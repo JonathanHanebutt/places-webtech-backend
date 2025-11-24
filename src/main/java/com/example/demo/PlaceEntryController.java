d037e06061689129bb79155ded3581beca609906
Jonathan Hanebutt <s0598131@htw-berlin.de>
2025-11-24 09:50:22 +0100

    Controller mit Listen füllen

--- src/main/java/com/example/demo/PlaceEntryController.java
+++ src/main/java/com/example/demo/PlaceEntryController.java
@@ -5,12 +5,39 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @CrossOrigin(origins = "*")
 public class PlaceEntryController {
 
     @GetMapping("/places")
-    public String allPlaces() {
-        return "B-)";
+    public List<PlaceEntry> allPlaces() {
+        return List.of(
+                new PlaceEntry(
+                        "HTW Berlin",
+                        "Campus Wilhelminenhof an der Spree",
+                        5,
+                        42,
+                        1,
+                        "/images/campus-wilhelminenhof.jpg"
+                ),
+                new PlaceEntry(
+                        "Tempelhofer Feld",
+                        "Ehemaliger Flughafen, heute Freizeitfläche",
+                        4,
+                        30,
+                        2,
+                        "/images/tempelhoferfeld.jpg"
+                ),
+                new PlaceEntry(
+                        "Tiergarten",
+                        "Großer Park im Herzen von Berlin",
+                        5,
+                        60,
+                        3,
+                        "/images/Tiergarten.jpeg"
+                )
+        );
     }
 }
