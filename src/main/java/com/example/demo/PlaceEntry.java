a50d27eaac3684df4e61db50aadc7c07c07584ac
Jonathan Hanebutt <s0598131@htw-berlin.de>
2025-11-24 10:04:14 +0100

    PlaceEntry change

--- src/main/java/com/example/demo/PlaceEntry.java
+++ src/main/java/com/example/demo/PlaceEntry.java
@@ -2,14 +2,14 @@ package com.example.demo;
 
 public class PlaceEntry {
 
-    String name;
-    String description;
-    int rating;
-    int like;
-    int dislike;
-    String imageLink;
+    private String name;
+    private String description;
+    private int rating;
+    private int like;
+    private int dislike;
+    private String imageLink;
 
-    PlaceEntry(){
+    public PlaceEntry() {
     }
 
     public PlaceEntry(String name, String description, int rating, int like, int dislike, String imageLink) {
@@ -21,4 +21,10 @@ public class PlaceEntry {
         this.imageLink = imageLink;
     }
 
+    public String getName() { return name; }
+    public String getDescription() { return description; }
+    public int getRating() { return rating; }
+    public int getLike() { return like; }
+    public int getDislike() { return dislike; }
+    public String getImageLink() { return imageLink; }
 }
